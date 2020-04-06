02127f9579292cf18877c82a92052b0095f83de7
fikri <me@fikri.tech>
2020-04-06 09:56:54 +0700

    rest

--- api_sekolahku/2_routers/pelajaran.js
+++ api_sekolahku/2_routers/pelajaran.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 
 Router.get('/')
 Router.get('/:id')
-Router.post('/:id')
+Router.post('/')
 Router.put('/:id')
 Router.patch('/:id')
 Router.delete('/:id')
