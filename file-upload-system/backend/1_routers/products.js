38ff31bf1d9080a05da8a1b8f3d0bd7aefb296ba
fikri <me@fikri.tech>
2020-04-07 11:30:50 +0700

    multer failed

--- file-upload-system/backend/1_routers/products.js
+++ file-upload-system/backend/1_routers/products.js
@@ -1,6 +1,48 @@
 const Router = require('express').Router()
 const controllers = require('./../2_controllers/products')
+const multer = require('multer')
+// const storageOptions = multer.diskStorage({
+//     destination : (req,file,cb) => {
+//         cb(null,'./../public/')
+//     },
+//     filename : (req,file,cb) => {
+//         cb(null,'PROD-IMG' + '-' + Date.now())
+//     }
+// })
+// const upload = multer({ dest: './../uploads/' }).single('product-image')
 
-Router.get('/' , controllers.getAllProducts)
+const storageConfig = multer.diskStorage({
+    // FILE MAU DISIMPAN DIMANA
+    destination : (req,file,cb) => {
+        // console.log(req.file)
+        cb(null , './uploads')
+    } ,
+    // NAMA FILE
+    filename : (req,file,cb) => {
+        cb(null , 'PRD-' + Date.now() + '.' + file.mimetype.split('/')[1])
+    } 
+})
+
+const filterConfig = (req, file, cb) => {
+    if(file.mimetype.split('/')[1] === 'png' || file.mimetype.split('/')[1] === 'jpeg'){
+        cb(null, true)
+    }else{
+        req.validation = {error : true , msg : 'File must be image'}
+        cb(null, false)
+    }
+}
+
+
+// STORAGE UNTUK MENGATUR PENYIMPANAN DAN NAMA FILE
+// UNTUK FILTERING JENIS FILE
+// UKURAN
+var upload = multer({storage : storageConfig , fileFilter : filterConfig})
+
+
+
+Router.get('/' , controllers.getAllProducts) 
+Router.get('/:id',controllers.getProductById)
+Router.post('/',upload.single('product') ,controllers.postNewProduct)
+
 
 module.exports = Router
--- file-upload-system/backend/2_controllers/products.js
+++ file-upload-system/backend/2_controllers/products.js
@@ -1,7 +1,11 @@
 const db = require('./../3_databases/mysql')
 
+
 const getAllProducts = (req,res) => {
-    const sql = 'select * from products;'
+    const sql = `select p.id as product_id, name, price, created_at, i.id as image_id, image_url 
+    from products p 
+    left join product_images i 
+    on p.id  = i.id_product;`
 
     db.query(sql, (err,result) => {
         try{
@@ -20,6 +24,37 @@ const getAllProducts = (req,res) => {
 }
 
 
+getProductById = (req,res) => {
+    const sql = `select p.id as product_id, name, price, created_at, i.id as image_id, image_url 
+    from products p 
+    left join product_images i 
+    on p.id  = i.id_product where p.id = ?;`
+    db.query(sql,req.params.id, (err,result) => {
+        try{
+            if(err) throw err
+            res.json({
+                error : false,
+                data : result[0]
+            })
+        }catch(err){
+            res.json({
+                error : true,
+                message : err.message
+            })
+        }
+    })
+}
+
+
+const postNewProduct = (req,res) => {
+    console.log(req.file)
+    console.log(req.files)
+    // post multiple image to api
+    // post ke dua table ==> product & product_image
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getProductById,
+    postNewProduct
 }
