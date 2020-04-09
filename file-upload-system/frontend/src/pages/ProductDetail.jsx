6c755c24d85aa97f8b452395363bea680d340a7f
fikri <me@fikri.tech>
2020-04-09 11:01:52 +0700

    product detail

--- file-upload-system/frontend/src/pages/ProductDetail.jsx
+++ file-upload-system/frontend/src/pages/ProductDetail.jsx
@@ -1,51 +1,47 @@
 import React, { Component } from 'react'
 import {Card} from 'antd'
+import Axios from 'axios'
 
 class ProductDetail extends Component {
+    state = {data : null}
+    componentDidMount(){
+        this.getDataProduct()
+    }
+    getDataProduct = () => {
+        Axios.get('http://localhost:4000/product/1')
+        .then((res) => {
+            console.log(res.data)
+            this.setState({data : res.data.data})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
     render() {
+        if(this.state.data === null) return <h1>Loading ...</h1>
+        if(this.state.data.length === 0) return <h1>Data Kosong</h1>
         return (
             <div className='container'>
                 <h1 className='text-center'>Product Detail Page</h1>
-                <h4 className='text-center'>Product Name</h4>
-                <h5 className='text-center'>Product Price</h5>
+                <h4 className='text-center'>{this.state.data[0].name}</h4>
+                <h5 className='text-center'>Rp. {this.state.data[0].price}</h5>
                 <div className='row justify-content-center'>
                     <div className='col-md-6' style={{height : '50vh'}}>
                         <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
+                            <img src={"http://localhost:4000/" + this.state.data[0].image_url }style={{height :"45vh",objectFit : "contain"}} width='100%' alt="broken"/>
                         </Card>
                     </div>
                 </div>
                 <div className='row mt-3'>
-                    <div className='col-md-2' style={{height : "120px"}}>
-                        <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
-                        </Card>
-                    </div>
-                    <div className='col-md-2' style={{height : "120px"}}>
-                        <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
-                        </Card>
-                    </div>
-                    <div className='col-md-2' style={{height : "120px"}}>
-                        <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
-                        </Card>
-                    </div>
-                    <div className='col-md-2' style={{height : "120px"}}>
-                        <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
-                        </Card>
-                    </div>
-                    <div className='col-md-2' style={{height : "120px"}}>
-                        <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
-                        </Card>
-                    </div>
-                    <div className='col-md-2' style={{height : "120px"}}>
-                        <Card className='h-100'>
-                            <img src="http://localhost:4000/public/PRD-IMG-1586398993692.jpeg" style={{height :"100%",objectFit : "cover"}} width='100%' alt="broken"/>
-                        </Card>
-                    </div>
+                   {this.state.data.slice(1).map((data) => {
+                       return(
+                        <div className='col-md-2' style={{height : "120px"}}>
+                            <Card className='h-100'>
+                                <img src={"http://localhost:4000/" + data.image_url} style={{height :"80px",objectFit : "contain"}} width='100%' alt="broken"/>
+                            </Card>
+                        </div>
+                       )
+                   })}
                 </div>
             </div>
         )
