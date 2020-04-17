4b018779bba02ff38cd524dcae65ce04196321fb
fikri <me@fikri.tech>
2020-04-17 09:32:02 +0700

    button state

--- auth/frontend/src/pages/Register.jsx
+++ auth/frontend/src/pages/Register.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import Axios from 'axios'
+// import LoadingOverlay from 'react-loading-overlay'
 
 export default class Register extends Component {
+    state={
+        loading : false
+    }
     onRegisterClick = () => {
+        this.setState({loading : true})
         const email = this.refs.email.value
         const password = this.refs.password.value
         const confirm = this.refs.confirm.value
@@ -19,9 +24,17 @@ export default class Register extends Component {
                     }else  if(res.data.error === true){
                         alert(res.data.message)
                     }
+                    this.refs.email.value = ''
+                    this.refs.password.value = ''
+                    this.refs.confirm.value = ''
+                    this.setState({loading : false})
                 })
                 .catch((err) => {
                     console.log(err)
+                    this.refs.email.value = ''
+                    this.refs.password.value = ''
+                    this.refs.confirm.value = ''
+                    this.setState({loading : false})
                 })
             }else{
                 return alert('password dan konfirm harus sama')
@@ -32,20 +45,28 @@ export default class Register extends Component {
     }
     render() {
         return (
-            <div className='container mt-5'>
-                <div className="row justify-content-center">
-                    <div className="col-md-4">
-                        <div className='card'>
-                            <div className="card-body">
-                                <input ref='email' className='form-control mt-3' placeholder='email' />
-                                <input ref='password' className='form-control mt-3' placeholder='password' />
-                                <input ref='confirm' className='form-control mt-3' placeholder='confirm your password' />
-                                <button onClick={this.onRegisterClick} className='btn btn-outline-primary mt-3'>Register</button>
+            // <LoadingOverlay active={true} spinner text='loading...'>
+                <div className='container mt-5'>
+                    <div className="row justify-content-center">
+                        <div className="col-md-4">
+                            <div className='card'>
+                                <div className="card-body">
+                                    <input ref='email' className='form-control mt-3' placeholder='email' />
+                                    <input ref='password' className='form-control mt-3' placeholder='password' />
+                                    <input ref='confirm' className='form-control mt-3' placeholder='confirm your password' />
+                                    {
+                                        this.state.loading === false ?
+                                        <button onClick={this.onRegisterClick} className='btn btn-outline-primary mt-3'>Register</button>
+                                        :
+                                        <button disabled className='btn btn-outline-primary mt-3'>Register</button>
+                                    }
+
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            // </LoadingOverlay>
         )
     }
 }
